c1408ebc8c67218640014f25629c91ed93b4f367
Sharan Khan <sharan.gohar@gmail.com>
2019-04-02 16:51:41 +0500

    completed track order screen

--- shopping-cart-app/src/screens/TrackOrder/TrackOrder.js
+++ shopping-cart-app/src/screens/TrackOrder/TrackOrder.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native'
 import styles from './styles'
-
+import BottomTab from '../../components/BottomTab/BottomTab'
+import Timeline from 'react-native-timeline-listview'
 /* Config/Constants
 ============================================================================= */
 
-
+const data = [
+    { time: '09:00', title: 'Order Received', description: '13:32, Nov 05,2019',lineColor:'#8CB65E'},
+    { time: '10:45', title: 'In process', description: '13:40, Nov 05,2019',lineColor:'#8CB65E'},
+    { time: '12:00', title: 'Ready for Delivery', description: '13:55, Nov 05,2019',lineColor:'#8CB65E'},
+    { time: '14:00', title: 'Picked up by the driver', description: '14:15, Nov 05,2019',lineColor:'#8CB65E'},
+    { time: '14:20', title: 'Delivered', description: '14:15, Nov 05,2019',lineColor:'#8CB65E'}
+];
 
 /* =============================================================================
 <TrackOrder />
@@ -19,7 +26,62 @@ class TrackOrder extends React.Component {
     render() {
         return (
             <View style={styles.flex}>
-                <Text>TrackOrder Working</Text>
+                <ScrollView>
+                    <View style={styles.headerContainer}>
+                        <Text style={styles.headerContainerText}>Order No. 10352</Text>
+                    </View>
+                    <View style={styles.profileContainer}>
+                        <View style={styles.profileContentContainer}>
+                            <Image
+                                source={require('../../assets/images/MainLanding/shop-1-avatar.png')}
+                                style={styles.imgFixed}
+                            />
+                            <View>
+                                <Text style={styles.profileTitlStyle}>Orders to</Text>
+                                <Text style={styles.profileSubTitleStyle}>ViVo H&T</Text>
+                            </View>
+
+                        </View>
+                        <TouchableOpacity style={styles.dotsContainer}>
+                            <View style={styles.dot}></View>
+                            <View style={styles.dot}></View>
+                            <View style={styles.dot}></View>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.cardContainer}>
+                        <View style={styles.card}>
+                            <View style={styles.cardLeftContainer}>
+                                <Image
+                                    source={require('../../assets/images/OrderDetail/basket.png')}
+                                    style={styles.imgResponsive}
+                                    resizeMode="cover"
+                                />
+                            </View>
+                            <View style={styles.cardRightContainer}>
+                                <Text style={[styles.productTitleStyle, styles.marginTop5]}>Leather Crossbody MID...</Text>
+                                <Text style={styles.productDescriptionStyle}>Courier received the order.</Text>
+                                <View style={styles.amountContainer}>
+                                    <View style={styles.quantityContainer}>
+                                        <Text style={styles.productTitleStyle}>x1</Text>
+                                    </View>
+                                    <View style={styles.priceContainer}>
+                                        <Text style={styles.productTitleStyle}>35 KWD</Text>
+                                    </View>
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.timelineContainer}>
+                        <Timeline
+                            data={data}
+                            circleSize={15}
+                            circleColor='#8CB65E'
+                            showTime={false}
+                            innerCircle={'dot'}
+                          />
+                    </View>
+                </ScrollView>
+                <BottomTab/>
             </View>
         )
     }
