4024678471edd1ad82eebf41409036f1ba81e90d
Sharan Khan <sharan.gohar@gmail.com>
2019-04-02 15:09:12 +0500

    completed order detail

--- shopping-cart-app/src/screens/OrderDetail/styles.js
+++ shopping-cart-app/src/screens/OrderDetail/styles.js
@@ -1,125 +1,216 @@
 import { moderateScale } from '../../utils/scaling'
 import { colors } from '../../utils/colors'
 import { fontStyles } from '../../utils/fontStyles';
-
+import { Dimensions } from 'react-native'
+const { height, width } = Dimensions.get('window')
 /* StyleSheet
 ============================================================================= */
 export default {
     flex: {
         flex:1
     },
-    orderContainer: {
-        height: '15%',
+    headerContainer: {
         width: '100%',
-        alignItems: 'flex-end',
-        borderBottomColor: colors.lightGrayColor,
+        height: height*.1,
+        justifyContent: 'flex-end',
+        borderColor: '#707070',
         borderWidth: moderateScale(1)
     },
-    orderSubContainer: {
-        height: '100%',
-        width: '95%',
-        justifyContent: 'center'
-    },
-    orderSubContainerTitle: {
-        height: '50%',
-        width: '100%',
-        justifyContent: 'flex-end'
-    },
-    orderSubContainerTitleText: {
-        fontFamily: fontStyles.PoppinsBold,
-        color: colors.primaryBlackColor,
-        fontSize: moderateScale(16)
-    },
-    orderSubContainerDes: {
-        height: '30%',
-        width: '100%',
-    },
-    orderSubContainerDesText: {
+    headerContainerText: {
+        marginBottom: '1%',
+        marginLeft: '5%',
         fontFamily: fontStyles.PoppinsRegular,
-        fontSize: moderateScale(12),
-        color: colors.fontGreyColor
+        fontSize: moderateScale(18)
     },
-    foodItemsContainer: {
-        height:'20%',
-        width:'100%',
-        alignItems:'flex-end',
-        borderBottomColor:colors.lightGrayColor,
-        borderWidth: moderateScale(1),
-        justifyContent: 'center'
-    },
-    foodItemsSubContainer: {
-        width:'95%',
-        height: '90%',
+    profileContainer: {
+        height: height*.1,
+        width: '100%',
         flexDirection: 'row',
-        justifyContent: 'space-around',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderColor: '#707070',
+        borderWidth: moderateScale(1)
     },
-    leftContainer: {
-        width: '25%',
-        height: '100%',
+    profileContentContainer: {
+        marginLeft: '5%',
+        width: '30%',
+        height: '80%',
+        flexDirection:'row',
+        alignItems: 'center',
+        justifyContent: 'space-around'
     },
-    smallText: {
+    imgFixed: {
+        width: moderateScale(40),
+        height: moderateScale(40)
+    },
+    profileTitlStyle: {
         fontFamily: fontStyles.PoppinsRegular,
+        color: '#4A4A4A',
         fontSize: moderateScale(12)
     },
-    priceText: {
-        fontSize: moderateScale(15),
+    profileSubTitleStyle: {
         fontFamily: fontStyles.PoppinsRegular,
-        color: colors.primaryOrangeColor
+        fontSize: moderateScale(14)
     },
-    middleContainer: {
-        width: '40%',
-        height: '100%',
-    },
-    rightContainer: {
-        width: '20%',
-        height: '100%',
+    dotsContainer: {
+        width: '30%',
+        height: '80%',
+        marginRight: '5%',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
         alignItems: 'center'
     },
-    timelineContainer: {
-        height: '35%',
+    dot: {
+        width: moderateScale(4),
+        height: moderateScale(4),
+        marginLeft: moderateScale(1),
+        borderRadius: moderateScale(2),
+        backgroundColor: colors.brownColor
+    },
+    cardContainer: {
+        backgroundColor: colors.backgroudGray,
         width: '100%',
-        alignItems: 'flex-end',
+        height: height*.2,
         justifyContent: 'center',
-        borderBottomColor:colors.lightGrayColor,
-        borderWidth: moderateScale(1)
-
+        alignItems: 'center',
+        borderBottomColor: '#9B9B9B',
+        borderWidth: 1
     },
-    timelineSubContainer: {
-        height: '90%',
-        width: '95%'
-    },
-    ratingContainer: {
-        backgroundColor: 'white',
-        height: '10%',
-        width: '100%',
-        alignItems: 'flex-end',
-        justifyContent: 'center',
-        borderBottomColor:colors.lightGrayColor,
-        borderWidth: moderateScale(1)
-    },
-    ratingSubContainer: {
-        height: '90%',
-        width: '95%',
+    card: {
+        backgroundColor: colors.whiteColor,
+        width: '90%',
+        height: '80%',
         flexDirection: 'row',
-        justifyContent: 'flex-start'
+        borderRadius: moderateScale(8),
+        borderTopLeftRadius: moderateScale(8),
+        borderBottomLeftRadius: moderateScale(8)
     },
-    ratingStarsContainer: {
-        justifyContent:'center',
-        width: '60%',
-        height: '100%',
-        alignItems:'flex-start',
-        marginLeft: '5%'
-
-    },
-    starsReviewContainer: {
+    cardLeftContainer: {
         width: '35%',
         height: '100%',
-        justifyContent: 'center',
-        alignItems: 'flex-start'
+        borderTopLeftRadius: moderateScale(8),
+        borderBottomLeftRadius: moderateScale(8)
     },
-    starsReviewContainerText: {
-        fontFamily: fontStyles.PoppinsBold,
+    cardRightContainer: {
+        marginLeft: '3%',
+        width: '60%',
+        height: '100%'
+    },
+    imgResponsive: {
+        flex: 1,
+        width: undefined,
+        height: undefined,
+        borderTopLeftRadius: moderateScale(8),
+        borderBottomLeftRadius: moderateScale(8)
+    },
+    marginTop5: {
+        marginTop: '5%'
+    },
+    productTitleStyle: {
+        fontFamily: fontStyles.PoppinsRegular,
+        fontSize: moderateScale(14)
+    },
+    productDescriptionStyle: {
+        fontFamily: fontStyles.PoppinsRegular,
         fontSize: moderateScale(12),
-        color: colors.fontBlackColor
+        color: colors.greenColor
+    },
+    amountContainer: {
+        width: '100%',
+        height: '30%',
+        flexDirection: 'row',
+        marginTop: '10%',
+        justifyContent: 'space-between'
+    },
+    quantityContainer: {
+        width: '40%',
+        height: '100%',
+        justifyContent: 'center'
+    },
+    priceContainer: {
+        width: '40%',
+        height: '100%',
+        justifyContent: 'center'
+    },
+    deliverContainer: {
+        width: '100%',
+        height: height*.4,
+        backgroundColor: colors.backgroudGray,
+        justifyContent: 'center',
+        borderBottomColor: '#9B9B9B',
+        borderWidth: 1
+    },
+    deliverSubContainer: {
+        marginLeft: '5%',
+        width: '50%',
+        height: '80%',
+    },
+    titleStyle: {
+        fontFamily: fontStyles.PoppinsRegular,
+        fontSize: moderateScale(16),
+        color: colors.brownColor
+    },
+    contactStyle: {
+        fontFamily: fontStyles.PoppinsRegular,
+        fontSize: moderateScale(14),
+        color: '#9B9B9B'
+    },
+    addressStyle: {
+        fontFamily: fontStyles.PoppinsRegular,
+        fontSize: moderateScale(14),
+    },
+    paymentContainer: {
+        width: '100%',
+        height: height*.2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderBottomColor: '#9B9B9B',
+        borderWidth: 1
+    },
+    paymentSubContainer: {
+        width: '90%',
+        height: '80%'
+    },
+    twoItems: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    totalContainer: {
+        width: '100%',
+        height: height*.1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    totalSubContainer: {
+        width: '90%',
+        height: '90%'
+    },
+    trackOrderContainer: {
+        width: '100%',
+        height: height*.2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: colors.backgroudGray
+    },
+    trackOrderSubContainer: {
+        width: '90%',
+        height: '50%',
+        backgroundColor: colors.whiteColor,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: moderateScale(4)
+    },
+    trackStyle: {
+        width: '90%',
+        height: '60%',
+        backgroundColor: colors.lightBlueColor,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: moderateScale(4)
+    },
+    trackStyleText: {
+        fontFamily: fontStyles.PoppinsRegular,
+        fontSize: moderateScale(16),
+        color: colors.whiteColor
     }
 }
--- shopping-cart-app/src/utils/colors.js
+++ shopping-cart-app/src/utils/colors.js
@@ -4,8 +4,10 @@ export  const colors = {
     facebookBlueColor: '#3B5998',
     googleRedColor: '#DC4E41',
     blueColor: '#2962FF', // TODO: SET THE COLOR FOR THE BLUE BUTTONS AND TEXT
-    backgroudGray: '#eee',
+    backgroudGray: '#F5F5F5',
     pinkColor: '#F48B98',
     textBlueColor: '#6178DE',
-    secondaryWhiteColor: '#fff'
+    secondaryWhiteColor: '#fff',
+    greenColor: '#8CB65E',
+    lightBlueColor: '#6178DE'
 }
