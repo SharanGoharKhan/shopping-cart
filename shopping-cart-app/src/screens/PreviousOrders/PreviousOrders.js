fec4f0fbd7b2ba211792cf3e45fb768412b50d55
Sharan Khan <sharan.gohar@gmail.com>
2019-04-03 12:42:06 +0500

    previousOrder Complete

--- shopping-cart-app/src/screens/PreviousOrders/PreviousOrders.js
+++ shopping-cart-app/src/screens/PreviousOrders/PreviousOrders.js
@@ -1,10 +1,68 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native'
 import styles from './styles'
-
+import BottomTab from '../../components/BottomTab/BottomTab'
 /* Config/Constants
 ============================================================================= */
-
+const cardData = [
+    {
+        date: '20 Aug, 2018 At 12:30 PM',
+        cards: [
+            {
+                title: 'Order No. 10363',
+                subTitle: 'Delivery Service',
+                status: 'In Delivery',
+                action: 'Review',
+                image: require('../../assets/images/MainLanding/carosel_img_3.png'),
+            },
+            {
+                title: 'Order No. 10352',
+                subTitle: 'Multiple Items/Shops',
+                status: 'In Delivery',
+                action: 'Review',
+                image: require('../../assets/images/MainLanding/shop-2-collage-2.png'),
+            },
+            {
+                title: 'Order No. 10363',
+                subTitle: 'Delivery Service',
+                status: 'In Delivery',
+                action: 'Review',
+                image: require('../../assets/images/MainLanding/carosel_img_3.png'),
+            }
+        ]
+    },
+    {
+        date: '15 Aug, 2018 At 12:30 PM',
+        cards: [
+            {
+                title: 'Order No. 10363',
+                subTitle: 'Delivery Service',
+                status: 'Delivered',
+                action: 'Review',
+                image: require('../../assets/images/MainLanding/carosel_img_3.png')
+            },
+            {
+                title: 'Order No. 10352',
+                subTitle: 'Multiple Items/Shops',
+                status: 'Delivered',
+                action: 'Review',
+                image: require('../../assets/images/MainLanding/shop-2-collage-2.png')
+            }
+        ]
+    },
+    {
+        date: '12 Aug, 2018 At 12:30 PM',
+        cards: [
+            {
+                title: 'Order No. 10387',
+                subTitle: 'Delivery Service',
+                status: 'Delivered',
+                action: 'Review',
+                image: require('../../assets/images/MainLanding/shop-2-collage-4.png')
+            }
+        ]
+    }
+]
 
 
 /* =============================================================================
@@ -19,7 +77,73 @@ class PreviousOrder extends React.Component {
     render() {
         return (
             <View style={styles.flex}>
-                <Text>PreviousOrder Working</Text>
+                <ScrollView>
+                    <View style={styles.headerContainer}>
+                        <Text style={styles.headerContainerText}>Previous Orders</Text>
+                    </View>
+                    <View style={styles.mainCardContainer}>
+                        {
+                            cardData.map((data, ind) =>
+                                <View key={ind}>
+                                    <View style={styles.dateContainer}>
+                                        <Text style={styles.subTtitleStyle}> On {data.date}</Text>
+                                    </View>
+                                    {
+                                        data.cards.map((card, ind) =>
+                                            <View
+                                                key={ind}
+                                                style={styles.cardContainer}>
+                                                <View style={styles.leftContainer}>
+                                                    <Image
+                                                        source={card.image}
+                                                        resizeMode="cover"
+                                                        style={[styles.imgResponsive, styles.roundedBorder]}
+                                                    />
+                                                </View>
+                                                <View style={styles.rightContainer}>
+                                                    <View style={styles.subRightContainer}>
+                                                        <View style={styles.titleContainer}>
+                                                            <Text style={styles.titleStyle}>{card.title}</Text>
+                                                            <View style={styles.rightArrowContainer}>
+                                                                <Image
+                                                                    source={require('../../assets/images/ProfileDashboard/rightArrow.png')}
+                                                                    resizeMode="contain"
+                                                                    style={styles.imgResponsive}
+                                                                />
+                                                            </View>
+                                                        </View>
+                                                        <View style={styles.subTitleContainer}>
+                                                            <Text style={styles.subTtitleStyle}>{card.subTitle}</Text>
+                                                        </View>
+                                                        <View style={styles.actionsContainer}>
+                                                            <View style={styles.subActionsContainer}>
+                                                                <Text style={styles.statusStyle}>{card.status}</Text>
+                                                                <TouchableOpacity style={styles.actionContainer}>
+                                                                    <Text style={styles.actionStyle}>{card.action}</Text>
+                                                                </TouchableOpacity>
+                                                            </View>
+                                                        </View>
+                                                    </View>
+                                                </View>
+                                            </View>
+                                        )
+                                    }
+                                    <View style={styles.lineContainer}>
+                                        {
+                                            ind < cardData.length - 1 ?
+                                                (
+                                                    <View style={styles.lineSubContainer}>
+                                                    </View>
+                                                )
+                                                : null
+                                        }
+                                    </View>
+                                </View>
+                            )
+                        }
+                    </View>
+                </ScrollView>
+                <BottomTab />
             </View>
         )
     }
--- shopping-cart-app/src/screens/PreviousOrders/styles.js
+++ shopping-cart-app/src/screens/PreviousOrders/styles.js
@@ -1,11 +1,136 @@
 import { moderateScale } from '../../utils/scaling'
 import { colors } from '../../utils/colors'
 import { fontStyles } from '../../utils/fontStyles';
-
+import { Dimensions } from 'react-native'
+const { height, width } = Dimensions.get('window')
 /* StyleSheet
 ============================================================================= */
 export default {
     flex: {
-        flex: 1
+        flex:1
+    },
+    headerContainer: {
+        width: '100%',
+        height: height*.1,
+        justifyContent: 'flex-end',
+        borderColor: '#707070',
+        borderWidth: moderateScale(1)
+    },
+    headerContainerText: {
+        marginBottom: '1%',
+        marginLeft: '5%',
+        fontFamily: fontStyles.PoppinsRegular,
+        fontSize: moderateScale(18)
+    },
+    dateContainer: {
+        height: height*.05,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    scrollViewContainer: {
+        flex: 1,
+    },
+    mainCardContainer: {
+        paddingTop: moderateScale(10),
+        alignItems: 'center',
+        backgroundColor: '#F5F5F5'
+    },
+    lineContainer: {
+        height: moderateScale(20),
+        justifyContent: 'center'
+    },
+    lineSubContainer: {
+        width: '100%',
+        height: 1,
+        backgroundColor: '#D8D8D8'
+    },
+    cardContainer: {
+        width: '90%',
+        backgroundColor: colors.whiteColor,
+        height: height*.14,
+        marginBottom: moderateScale(4),
+        marginTop: moderateScale(4),
+        borderRadius: moderateScale(8),
+        flexDirection: 'row',
+        borderWidth: moderateScale(1),
+        borderColor: '#EEEEEE',
+    },
+    leftContainer: {
+        width: '40%',
+        height: '100%'
+    },
+    imgResponsive: {
+        flex: 1,
+        width: undefined,
+        height: undefined
+    },
+    roundedBorder: {
+        borderRadius: moderateScale(8),
+        borderTopRightRadius:0,
+        borderBottomRightRadius:0
+    },
+    rightContainer: {
+        marginLeft: '5%',
+        width: '55%',
+        height: '100%',
+        justifyContent: 'center'
+    },
+    subRightContainer: {
+        width:'95%',
+        height: '90%'
+    },
+    titleContainer: {
+        width:'100%',
+        height: '30%',
+        flexDirection:'row',
+        justifyContent: 'space-between'
+    },
+    titleStyle: {
+        fontFamily: fontStyles.PoppinsRegular,
+        fontSize: moderateScale(14)
+    },
+    rightArrowContainer: {
+        width: '6%',
+        height: '60%'
+    },
+    subTitleContainer: {
+        width: '100%',
+        height: '20%'
+    },
+    subTtitleStyle: {
+        fontFamily: fontStyles.PoppinsRegular,
+        fontSize: moderateScale(12),
+        color: '#9B9B9B'
+    },
+    actionsContainer: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    },
+    subActionsContainer: {
+        width: '100%',
+        height: '80%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+
+    },
+    statusStyle: {
+        fontFamily: fontStyles.PoppinsRegular,
+        fontSize: moderateScale(12),
+        color: colors.blueColor
+    },
+    actionContainer: {
+        width: '40%',
+        height: '80%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: '5%',
+        borderRadius:moderateScale(4),
+        backgroundColor: colors.textBlueColor
+    },
+    actionStyle: {
+        fontFamily: fontStyles.PoppinsRegular,
+        fontSize: moderateScale(12),
+        color: colors.whiteColor
     }
 }
