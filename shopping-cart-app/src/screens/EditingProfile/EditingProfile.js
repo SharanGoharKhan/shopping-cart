4acb250e088910e677d88975297bac470b95cead
Sharan Khan <sharan.gohar@gmail.com>
2019-04-03 16:22:19 +0500

    editing profile complete

--- shopping-cart-app/src/screens/EditingProfile/EditingProfile.js
+++ shopping-cart-app/src/screens/EditingProfile/EditingProfile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, TextInput, KeyboardAvoidingView } from 'react-native'
 import styles from './styles'
 
 /* Config/Constants
@@ -19,7 +19,98 @@ class EditingProfile extends React.Component {
     render() {
         return (
             <View style={styles.flex}>
-                <Text>EditingProfile Working</Text>
+                <View style={styles.headerContainer}>
+                    <Text style={styles.headerContainerText}>Editing Profile</Text>
+                </View>
+                <View style={styles.formMainContainer}>
+                    <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+                        <View style={styles.formContainer}>
+                            <View style={styles.profileImageContainer}>
+                                <Image
+                                    source={require('../../assets/images/mainMenu/menu_avatar.png')}
+                                    style={styles.imgResponsive}
+                                    resizeMode="contain"
+                                />
+                            </View>
+                            <View style={styles.formContentContainer}>
+                                <View style={styles.twoItemsContainer}>
+                                    <View style={styles.halfContainer}>
+                                        <View style={styles.labelContainer}>
+                                            <Text style={styles.label}>First Name</Text>
+                                        </View>
+                                        <View style={styles.inputContainer}>
+                                            <TextInput
+                                                placeholder="Sharan"
+                                                placeholderTextColor='black'
+                                            />
+                                        </View>
+                                    </View>
+                                    <View style={styles.halfContainer}>
+                                        <View style={styles.labelContainer}>
+                                            <Text style={styles.label}>Last Name</Text>
+                                        </View>
+                                        <View style={styles.inputContainer}>
+                                            <TextInput
+                                                placeholder="Khan"
+                                                placeholderTextColor='black'
+                                            />
+                                        </View>
+                                    </View>
+                                </View>
+                                <View style={styles.oneItemContainer}>
+                                    <View style={styles.fullContainer}>
+                                        <View style={styles.labelContainer}>
+                                            <Text style={styles.label}>Email</Text>
+                                        </View>
+                                        <View style={styles.inputContainer}>
+                                            <TextInput
+                                                placeholder="sharan.gohar@gmail.com"
+                                                placeholderTextColor="black"
+                                            />
+                                        </View>
+                                    </View>
+                                </View>
+                                <View style={styles.oneItemContainer}>
+                                    <View style={styles.fullContainer}>
+                                        <View style={styles.labelContainer}>
+                                            <Text style={styles.label}>Phone Number</Text>
+                                        </View>
+                                        <View style={styles.inputContainer}>
+                                            <TextInput
+                                                placeholder="+92 3339461270"
+                                                placeholderTextColor="black"
+                                            />
+                                        </View>
+                                    </View>
+                                </View>
+                                <View style={styles.twoItemsContainer}>
+                                    <View style={styles.halfContainer}>
+                                        <View style={styles.labelContainer}>
+                                            <Text style={styles.label}>Date of Birth</Text>
+                                        </View>
+                                        <View style={styles.inputContainer}>
+                                            <TextInput
+                                                placeholder="16-06-1993"
+                                                placeholderTextColor="black"
+                                            />
+                                        </View>
+                                    </View>
+                                    <View style={styles.halfContainer}>
+                                        <View style={styles.labelContainer}>
+                                            <Text style={styles.label}>Gender</Text>
+                                        </View>
+                                        <View style={styles.inputContainer}>
+                                            <TextInput
+                                                placeholder="Male"
+                                                placeholderTextColor="black"
+                                            />
+                                        </View>
+                                    </View>
+                                </View>
+                            </View>
+                        </View>
+                    </KeyboardAvoidingView>
+                </View>
             </View>
         )
     }
--- shopping-cart-app/src/screens/EditingProfile/styles.js
+++ shopping-cart-app/src/screens/EditingProfile/styles.js
@@ -1,11 +1,96 @@
 import { moderateScale } from '../../utils/scaling'
 import { colors } from '../../utils/colors'
 import { fontStyles } from '../../utils/fontStyles';
-
+import { Dimensions } from 'react-native'
+const { height, width } = Dimensions.get('window')
 /* StyleSheet
 ============================================================================= */
 export default {
     flex: {
         flex: 1
+    },
+    headerContainer: {
+        width: '100%',
+        height: '10%',
+        justifyContent: 'flex-end',
+        borderColor: '#707070',
+        borderWidth: moderateScale(1)
+    },
+    headerContainerText: {
+        marginBottom: '1%',
+        marginLeft: '5%',
+        fontFamily: fontStyles.PoppinsRegular,
+        fontSize: moderateScale(18)
+    },
+    formMainContainer: {
+        flex: 1,
+        backgroundColor: colors.backgroudGray,
+        alignItems: 'center'
+    },
+    formContainer: {
+        marginTop: '15%',
+        width: '90%',
+        height: '80%',
+        backgroundColor: colors.whiteColor,
+        borderRadius: moderateScale(8),
+        alignItems: 'center',
+        position: 'relative'
+    },
+    profileImageContainer: {
+        width: moderateScale(80),
+        height: moderateScale(80),
+        position: 'absolute',
+        alignSelf: 'center',
+        backgroundColor: colors.whiteColor,
+        borderRadius: moderateScale(40),
+        top: moderateScale(-40),
+        borderWidth: moderateScale(3),
+        borderColor: '#DDDDDD',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    imgResponsive: {
+        width: '60%',
+        height: '60%'
+    },
+    formContentContainer: {
+        marginTop: '15%',
+        width: '95%',
+        height: '80%'
+    },
+    twoItemsContainer: {
+        width: '100%',
+        height: '20%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    halfContainer: {
+        width: '45%',
+        height: '80%'
+    },
+    labelContainer: {
+        width: '100%',
+        height: '40%'
+    },
+    inputContainer: {
+        width: '100%',
+        height: '60%',
+        backgroundColor: colors.backgroudGray
+    },
+    label: {
+        fontFamily: fontStyles.PoppinsRegular,
+        color: colors.darkGrayText,
+        fontSize: moderateScale(14)
+    },
+    oneItemContainer: {
+        width: '100%',
+        height: '20%',
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    fullContainer: {
+        width: '95%',
+        height: '80%'
     }
 }
