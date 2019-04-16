31ac95ce8d3a34e1e97691661bbd27735fcccde4
Sharan Khan <sharan.gohar@gmail.com>
2019-04-16 15:03:20 +0500

    added background image signin

--- shopping-cart-app/src/screens/SignIn/SignIn.js
+++ shopping-cart-app/src/screens/SignIn/SignIn.js
@@ -44,23 +44,23 @@ class SignIn extends React.Component {
         return (
             <View style={styles.container}>
                 <StatusBar backgroundColor="transparent" barStyle="light-content" hidden={false} translucent={false} />
-            <ForgotPassword
+                <ForgotPassword
                     modalVisible={this.state.modalVisible}
-                  hideModal={this.hideModal}
+                    hideModal={this.hideModal}
                 />
                 <View style={styles.statusBarImage}>
-                <Image
-                    source={require('../../assets/images/statusbar.png')}
-                    style={{
-                            height: Platform.OS =='ios' ? 20 : StatusBar.currentHeight,
+                    <Image
+                        source={require('../../assets/images/statusbar.png')}
+                        style={{
+                            height: Platform.OS == 'ios' ? 20 : StatusBar.currentHeight,
                             width: '100%',
                         }}
-                  />
-              </View>
-            <View style={styles.body}>
-                <View style={styles.body_header}>
-                      <Image
-                          source={require('../../assets/icons/avatar.png')}
+                    />
+                </View>
+                <View style={styles.body}>
+                    <View style={styles.body_header}>
+                        <Image
+                            source={require('../../assets/icons/avatar.png')}
                             style={{
                                 height: moderateScale(18),
                                 width: moderateScale(16),
@@ -68,12 +68,12 @@ class SignIn extends React.Component {
                         />
                         <Text style={styles.header_text}>Sign In</Text>
                     </View>
-                <View style={styles.body_container}>
+                    <View style={styles.body_container}>
                         <ImageBackground
                             style={styles.body_container_background}
                             source={require('../../assets/images/formBackground.png')}
-                  >
-                    <View style={styles.bc_texts}>
+                        >
+                            <View style={styles.bc_texts}>
                                 <Text style={styles.bc_h1}>
                                     Hello Guest!
                       </Text>
@@ -81,85 +81,85 @@ class SignIn extends React.Component {
                                     Sign In
                       </Text>
                             </View>
-                    <View style={styles.bc_main}>
+                            <View style={styles.bc_main}>
                                 <TextField
                                     placeholderText="Username"
-                      />
-                            <TextField
-                                  placeholderText="Password"
                                 />
-                            <MainBtn
-                                  onPress={() => this.props.navigation.navigate('MainLanding')}
-                                  text="Sign In"
+                                <TextField
+                                    placeholderText="Password"
                                 />
-                            <TouchableOpacity onPress={() => this.showModal()}>
-                        <Text style={styles.passwordText}>
-Forgot my password
+                                <MainBtn
+                                    onPress={() => this.props.navigation.navigate('MainLanding')}
+                                    text="Sign In"
+                                />
+                                <TouchableOpacity onPress={() => this.showModal()}>
+                                    <Text style={styles.passwordText}>
+                                        Forgot my password
                                   </Text>
-                      </TouchableOpacity>
-                          </View>
+                                </TouchableOpacity>
+                            </View>
                             <View style={styles.bc_socialBtns}>
                                 <View style={[styles.socialBtns_View, styles.google_btn]}>
                                     <TouchableOpacity
                                         onPress={() => this.props.navigation.navigate('MainLanding')}
                                         style={styles.socialBtn}
-                                  >
+                                    >
                                         <Image
-                                        source={require('../../assets/icons/google.png')}
+                                            source={require('../../assets/icons/google.png')}
                                             style={{
                                                 height: moderateScale(25),
                                                 width: moderateScale(25),
                                             }}
-                                      />
-                                    <Text style={styles.fb_text}>Google</Text>
-                                  </TouchableOpacity>
-                      </View>
+                                        />
+                                        <Text style={styles.fb_text}>Google</Text>
+                                    </TouchableOpacity>
+                                </View>
                                 <View style={[styles.socialBtns_View, styles.facebook_btn]}>
                                     <TouchableOpacity
-                                    onPress={() => this.props.navigation.navigate('MainLanding')}
-                                    style={styles.socialBtn}
-                                  >
+                                        onPress={() => this.props.navigation.navigate('MainLanding')}
+                                        style={styles.socialBtn}
+                                    >
                                         <Image
-                                        source={require('../../assets/icons/facebook.png')}
-                                        style={{
+                                            source={require('../../assets/icons/facebook.png')}
+                                            style={{
                                                 height: moderateScale(24),
                                                 width: moderateScale(24),
                                             }}
-                                      />
-                                    <Text style={styles.fb_text}>Facebook</Text>
-                                  </TouchableOpacity>
-                      </View>
-                          </View>
-                  </ImageBackground>
+                                        />
+                                        <Text style={styles.fb_text}>Facebook</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </ImageBackground>
                     </View>
                     <View style={styles.body_footer}>
                         <View style={styles.footer}>
                             <View style={styles.mixed_line}>
-                            <Text style={[styles.ft_text, styles.bold_text]}>Create New Account: </Text>
+                                <Text style={[styles.ft_text, styles.bold_text]}>Create New Account: </Text>
                                 <Text style={styles.ft_text}>By creating a new account, you</Text>
-                          </View>
-                        <Text style={styles.ft_text}>
+                            </View>
+                            <Text style={styles.ft_text}>
                                 will become a valuable customer/buyer to Enatega by
                             </Text>
                             <View style={styles.mixed_line}>
-                            <Text style={styles.ft_text}>default. While registering, </Text>
-                            <Text
+                                <Text style={styles.ft_text}>default. While registering, </Text>
+                                <Text
                                     onPress={() => this.props.navigation.navigate('SignUp')}
                                     style={[styles.ft_underline, styles.ft_text]}
-                              >you can also choose to
+                                >you can also choose to
                                 </Text>
-                          </View>
+                            </View>
                             <Text
                                 onPress={() => this.props.navigation.navigate('SignUp')}
                                 style={[styles.ft_underline, styles.ft_text]}
-                          >
+                            >
                                 become a seller and open your own shop
                           </Text>
-                      </View>
-              </View>
-              </View>
+                        </View>
+                    </View>
+                </View>
 
-          </View>
+            </View>
         );
     }
 }
