b112c837fd1a95cb8724d9079f7d0fbd8026cac2
Sharan Gohar Khan <sharan.gohar@gmail.com>
2020-01-21 13:55:09 +0500

    safe area view fix

--- shopping-cart-app/App.js
+++ shopping-cart-app/App.js
@@ -19,7 +19,6 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1, 
-    marginTop: Platform.OS === 'ios' ? 0 : -(StatusBar.currentHeight)
   },
   spinnerContainer: {
     flex: 1, 
