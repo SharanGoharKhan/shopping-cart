af21321da2acdad78cecb66970374c920a81e6a9
saadi-ninjasCode <saadjaved79@gmail.com>
2020-02-26 18:48:08 +0500

    profile dashboard hooks

--- shopping-cart-app/src/screens/ProfileDashboard/ProfileDashboard.js
+++ shopping-cart-app/src/screens/ProfileDashboard/ProfileDashboard.js
@@ -7,34 +7,32 @@ import ProfileContainer from './ProfileContainer/ProfileContainer';
 import BottomTab from '../../components/BottomTab/BottomTab';
 import CardContainer from './CardContainer/CardContainer';
 
-class ProfileDashboard extends React.Component {
-  render() {
+function ProfileDashboard(props){
     return (
       <React.Fragment>
         <View style={styles.flex}>
           <ProfileContainer
-            navigationObj={this.props.navigation}
+            navigationObj={props.navigation}
           />
           <View style={styles.tabContainer}>
             <TouchableOpacity>
               <Text style={styles.tabStyleActive}>My Active Orders (3)</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              onPress={() => this.props.navigation.navigate('PreviousOrders')}
+              onPress={() => props.navigation.navigate('PreviousOrders')}
             >
               <Text style={styles.tabStyle}>Previous Orders (35)</Text>
             </TouchableOpacity>
           </View>
           <CardContainer
-            navigationObj={this.props.navigation}
+            navigationObj={props.navigation}
           />
         </View>
         <BottomTab
-          navigationObj={this.props.navigation}
+          navigationObj={props.navigation}
         />
       </React.Fragment>
     );
-  }
 }
 
 export default ProfileDashboard;
