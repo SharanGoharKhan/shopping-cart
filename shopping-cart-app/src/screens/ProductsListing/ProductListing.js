08300ba9d9d30d79c939557b353b40d18b976069
saadi-ninjasCode <saadjaved79@gmail.com>
2020-02-26 21:40:39 +0500

    ..

--- shopping-cart-app/src/screens/ProductsListing/ProductListing.js
+++ shopping-cart-app/src/screens/ProductsListing/ProductListing.js
@@ -6,62 +6,60 @@ import styles from './styles';
 import CardItem from './CardItem/CardItem';
 import BottomTab from '../../components/BottomTab/BottomTab';
 
-class ProductListing extends React.Component {
-    render() {
-        return (
-            <View style={styles.flex}>
-                <View style={styles.headerContainer}>
-                    <View style={styles.headerSubContainer}>
-                        <View style={styles.backCnt}>
-                            <TouchableOpacity
-                                onPress={() => this.props.navigation.goBack()}
-                                style={styles.backImg}>
-                                <Image
-                                    source={require('../../assets/icons/back.png')}
-                                    resizeMode="contain"
-                                    style={styles.imgResponsive}
-                                />
-                            </TouchableOpacity>
-                        </View>
-                        <View style={styles.headerTextContainer}>
-                            <Text style={styles.headerText}>Arts & Crafts</Text>
-                        </View>
-                        <View style={styles.headerIconContainer}>
-                            <TouchableOpacity style={styles.headerImg}>
-                                <Image
-                                    source={require('../../assets/images/ProductListing/listView.png')}
-                                    resizeMode="contain"
-                                    style={styles.imgResponsive}
-                                />
-                            </TouchableOpacity>
-                            <TouchableOpacity style={styles.headerImg}>
-                                <Image
-                                    source={require('../../assets/images/ProductListing/settings.png')}
-                                    resizeMode="contain"
-                                    style={styles.imgResponsive}
-                                />
-                            </TouchableOpacity>
-                        </View>
+function ProductListing(props) {
+    return (
+        <View style={styles.flex}>
+            <View style={styles.headerContainer}>
+                <View style={styles.headerSubContainer}>
+                    <View style={styles.backCnt}>
+                        <TouchableOpacity
+                            onPress={() => props.navigation.goBack()}
+                            style={styles.backImg}>
+                            <Image
+                                source={require('../../assets/icons/back.png')}
+                                resizeMode="contain"
+                                style={styles.imgResponsive}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.headerTextContainer}>
+                        <Text style={styles.headerText}>Arts & Crafts</Text>
+                    </View>
+                    <View style={styles.headerIconContainer}>
+                        <TouchableOpacity style={styles.headerImg}>
+                            <Image
+                                source={require('../../assets/images/ProductListing/listView.png')}
+                                resizeMode="contain"
+                                style={styles.imgResponsive}
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.headerImg}>
+                            <Image
+                                source={require('../../assets/images/ProductListing/settings.png')}
+                                resizeMode="contain"
+                                style={styles.imgResponsive}
+                            />
+                        </TouchableOpacity>
                     </View>
                 </View>
-                <ScrollView style={styles.scrollViewStyle}>
-                    <View style={styles.bannerContainer}>
-                        <Image
-                            source={require('../../assets/images/ProductListing/banner.png')}
-                            resizeMode="cover"
-                            style={styles.imgResponsive}
-                        />
-                    </View>
-                    <CardItem
-                        navigationObj={this.props.navigation}
-                    />
-                </ScrollView>
-                <BottomTab
-                    navigationObj={this.props.navigation}
-                />
             </View>
-        );
-    }
+            <ScrollView style={styles.scrollViewStyle}>
+                <View style={styles.bannerContainer}>
+                    <Image
+                        source={require('../../assets/images/ProductListing/banner.png')}
+                        resizeMode="cover"
+                        style={styles.imgResponsive}
+                    />
+                </View>
+                <CardItem
+                    navigationObj={props.navigation}
+                />
+            </ScrollView>
+            <BottomTab
+                navigationObj={props.navigation}
+            />
+        </View>
+    );
 }
 
 export default ProductListing;
