9bc8061832293d720b7bf6cc44d9f168d94a157a
saadi-ninjasCode <saadjaved79@gmail.com>
2020-02-27 13:31:32 +0500

    Fonst added

--- shopping-cart-app/App.js
+++ shopping-cart-app/App.js
@@ -1,28 +1,52 @@
-import React from 'react'
-import { View, StatusBar, StyleSheet, Platform } from 'react-native'
-import { SafeAreaView } from 'react-navigation'
+import React, { useState, useEffect } from 'react'
 import AppContainer from './src/utils/routes'
+import * as Font from 'expo-font'
+import { View, ActivityIndicator, StatusBar, StyleSheet, Platform } from 'react-native'
+import { colors } from './src/utils/colors'
+import { SafeAreaView } from 'react-navigation';
 
-function App() {
-  return (
-    <SafeAreaView forceInset={{ top: 'always' }} style={styles.flex}>
-      <View style={styles.flex}>
-        <StatusBar backgroundColor='white' barStyle='dark-content'/>
-        <AppContainer />
-      </View>
-    </SafeAreaView>
+export default function App() {
+  const [fontLoaded, fontLoadedSetter] = useState(false);
+
+  useEffect(() => {
+    loadApp();
+  })
+
+  async function loadApp() {
+    await Font.loadAsync({
+      'Poppins-Regular': require('./src/assets/font/Poppins/Poppins-Regular.ttf'),
+      'Poppins-Bold': require('./src/assets/font/Poppins/Poppins-Bold.ttf')
+    })
+    fontLoadedSetter(true)
+  }
+
+  if (fontLoaded) {
+    //testing 4
+    return (
+      <SafeAreaView forceInset={{ top: 'always' }} style={styles.flex}>
+        <View style={styles.container}>
+          <AppContainer />
+        </View>
+      </SafeAreaView>
+    )
+  } else return (
+    <View style={styles.spinnerContainer}>
+      {fontLoaded}
+      <ActivityIndicator size="large" color={colors.greenColor} />
+    </View>
   )
 }
-
 const styles = StyleSheet.create({
   flex: {
     flex: 1
   },
+  container: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -(StatusBar.currentHeight)
+  },
   spinnerContainer: {
-    flex: 1, 
-    justifyContent: 'center', 
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center'
   }
 })
-
-export default App;
