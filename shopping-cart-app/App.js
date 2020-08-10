e714aadc970aa42d82a9c6006b5efe0a0b161bd6
saadi-ninjasCode <saadjaved79@gmail.com>
2020-08-10 15:44:00 +0500

    app -> Linking Apollo

--- shopping-cart-app/App.js
+++ shopping-cart-app/App.js
@@ -1,31 +1,28 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import AppContainer from './src/routes/routes'
-import * as Font from 'expo-font'
-import { View, ActivityIndicator, StatusBar, StyleSheet, Platform } from 'react-native'
+import { View, ActivityIndicator, StatusBar, StyleSheet } from 'react-native'
 import { colors } from './src/utils/colors'
+import { useFonts } from 'expo-font';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 
 export default function App() {
-  const [fontLoaded, fontLoadedSetter] = useState(false);
+  const [fontLoaded] = useFonts({
+    'Poppins-Regular': require('./src/assets/font/Poppins/Poppins-Regular.ttf'),
+    'Poppins-Bold': require('./src/assets/font/Poppins/Poppins-Bold.ttf')
+  });
 
-  useEffect(() => {
-    loadApp();
-  })
-
-  async function loadApp() {
-    await Font.loadAsync({
-      'Poppins-Regular': require('./src/assets/font/Poppins/Poppins-Regular.ttf'),
-      'Poppins-Bold': require('./src/assets/font/Poppins/Poppins-Bold.ttf')
-    })
-    fontLoadedSetter(true)
-  }
+  const client = new ApolloClient({
+    // uri: 'https://48p1r2roz4.sse.codesandbox.io',
+    cache: new InMemoryCache()
+  });
 
   if (fontLoaded) {
     return (
-      <>
+      <ApolloProvider client={client}>
         <StatusBar barStyle={'dark-content'} backgroundColor={colors.headerbackground} />
         <AppContainer />
-      </>
+      </ApolloProvider>
     )
   } else return (
     <View style={styles.spinnerContainer}>
