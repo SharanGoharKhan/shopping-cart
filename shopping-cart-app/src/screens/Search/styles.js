45bac37d83c5cf17d18c76aa7468637a36c11c4c
saadi-ninjasCode <saadjaved79@gmail.com>
2020-08-20 16:35:44 +0500

    app-> search screen updated

--- shopping-cart-app/src/screens/Search/styles.js
+++ shopping-cart-app/src/screens/Search/styles.js
@@ -9,34 +9,15 @@ export default {
     safeAreaStyle: {
         backgroundColor: colors.headerbackground
     },
-    mainContainer: {
-        marginTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
-    },
     container: {
         height: '100%',
         width: '100%',
         alignSelf: 'center',
     },
-    statusbarImage: {
-        height: StatusBar.currentHeight,
-        backgroundColor: 'black',
-    },
     body: {
         height: '95%',
         width: '100%',
         alignSelf: 'center',
-    },
-    main_scroller: {
-        marginBottom: verticalScale(10),
-    },
-
-    // header
-    header: {
-        height: height * .08,
-        width: '95%',
-        flexDirection: 'row',
-        alignSelf: 'center',
-        alignItems: 'center',
     },
     // main
     main: {
