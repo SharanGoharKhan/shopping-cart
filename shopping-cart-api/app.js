0e3109f65973c15b1db4e020cca4f2115e7138f2
danyal malik <01-234122-029@student.bahria.edu.pk>
2020-08-26 12:48:09 +0500

    admin panel redirection issue fixed

--- shopping-cart-api/app.js
+++ shopping-cart-api/app.js
@@ -70,9 +70,9 @@ app.get('/chat', function (req, res) {
 })
 
 app.use('/dashboard', express.static(path.join(__dirname, '/build')))
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname + '/build/index.html'))
-// })
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'))
+})
 
 const server = new ApolloServer({
   typeDefs,
